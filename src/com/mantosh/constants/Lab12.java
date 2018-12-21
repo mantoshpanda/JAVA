9eb1dd42276741a311a0f40a147fe31cab3be2ab
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 22:19:42 +0530

    Constant is also called as Final Variable

---
+++ src/com/mantosh/constants/Lab12.java
@@ -0,0 +1,11 @@
+package com.mantosh.constants;
+
+public class Lab12 {
+    public static void main(String as[]){
+        final int A = 99;
+        System.out.println(A);
+        /* A=88;
+        System.out.println(A); //java cannot assign a value to a final variable A
+        */ //uncommenting this will throw the above error
+    }
+}
