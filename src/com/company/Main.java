1cf267034816fafa6dbb183398c6143656f2d01e
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 13:32:02 +0530

    My First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a;
+	//Uncommenting the below line will result in compilation error. variable a is not initialize
+        // System.out.println(a);
+    }
+}
