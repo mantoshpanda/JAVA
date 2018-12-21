ed18b742dc46ebbfc1f1004cfb5e82f714f5427b
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 16:02:05 +0530

    Declaration of Static Variable

---
+++ src/com/mantosh/variable_declaration/Lab4.java
@@ -0,0 +1,8 @@
+package com.mantosh.variable_declaration;
+
+public class Lab4 {
+    static int a ;
+    public static void main(String[] as) {
+        System.out.println(a); //gives the output as 0  "because the default value of static variable is 0"
+    }
+}
