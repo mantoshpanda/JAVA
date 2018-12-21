a773900463a83cbc01b3c3ac57b9b5fe681ea8ea
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 21:58:16 +0530

    Constant - Special Variable

---
+++ src/com/mantosh/constants/Lab11.java
@@ -0,0 +1,10 @@
+package com.mantosh.constants;
+
+public class Lab11 {
+    public static void main(String... arg){
+        int a = 99;
+        System.out.println(a);
+        a=88;
+        System.out.println(a);
+    }
+}
