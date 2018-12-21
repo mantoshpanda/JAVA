0746330863a1ca753e72567f278a7b079238ebea
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 17:45:33 +0530

    Declaring and Initializing a single variable in one variable declaration statement

---
+++ src/com/mantosh/variable_declaration/Lab7.java
@@ -0,0 +1,16 @@
+package com.mantosh.variable_declaration;
+
+//declaring and initializing a single variable in one variable declaration statement
+
+public class Lab7 {
+    public static void main (String as[]){
+        int a = 99;
+        boolean b = true;
+        double d = 999.99;
+        String str = "Mantosh";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println(str);
+    }
+}
