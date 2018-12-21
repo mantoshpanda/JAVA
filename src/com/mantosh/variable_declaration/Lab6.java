b6c11254665b335a7277216b08e7954664284e52
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 16:51:37 +0530

    Initializing a single variable

--- src/com/mantosh/variable_declaration/Lab6.java
+++ src/com/mantosh/variable_declaration/Lab6.java
@@ -1,8 +1,12 @@
 package com.mantosh.variable_declaration;
 
-public class Lab5 {
-    int a;
-    public static void main(String[] as){
-                System.out.println(a); //non-static field 'a' cannot be referenced from a static context
+public class Lab6 {
+        public static void main(String[] as){
+            String str;
+            int a;
+            a=99;
+            str="MP";
+            System.out.println(a);
+            System.out.println(str);
     }
 }
