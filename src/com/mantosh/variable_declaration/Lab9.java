be10508a5913daf66206eb0958bf41e573985db0
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 17:59:37 +0530

    Initializing multiple variables with same value

---
+++ src/com/mantosh/variable_declaration/Lab9.java
@@ -0,0 +1,16 @@
+package com.mantosh.variable_declaration;
+
+public class Lab9 {
+    public static void main(String[] as){
+        int a,b,c;
+        a=b=c=99;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        String str1, str2, str3;
+        str1=str2=str3="Mantosh";
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
