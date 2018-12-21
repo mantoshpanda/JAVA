9d420200e38011c494724ffd23a84f738d0cce28
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 16:15:31 +0530

    Non Static variable cannot be referenced from a static context

---
+++ src/com/mantosh/variable_declaration/Lab5.java
@@ -0,0 +1,8 @@
+package com.mantosh.variable_declaration;
+
+public class Lab5 {
+    int a;
+    public static void main(String[] as){
+        // System.out.println(a); // non-static variable a cannot be reference from a static context
+    }
+}
