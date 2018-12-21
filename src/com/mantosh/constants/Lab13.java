4f9e3aa10d5da0f3c52c80c23453b2c4274c74f6
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 22:27:22 +0530

    Initializing a Final variable

---
+++ src/com/mantosh/constants/Lab13.java
@@ -0,0 +1,11 @@
+package com.mantosh.constants;
+
+public class Lab13 {
+    public static void main(String... arg){
+        /*
+        final int A; //variable A might not have been initialized
+        System.out.println(A);
+        */
+        //uncommenting this will throw an error variable A might not have been initialized
+    }
+}
