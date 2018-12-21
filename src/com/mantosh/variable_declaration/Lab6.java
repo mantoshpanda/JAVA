6f5462091ce35f04e2964a2dbaf50fdae66eb2a0
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 16:41:40 +0530

    Initializing a single variable

---
+++ src/com/mantosh/variable_declaration/Lab6.java
@@ -0,0 +1,12 @@
+package com.mantosh.variable_declaration;
+
+public class Lab6 {
+    public static void main(String[] as){
+        String str;
+        int a;
+        str = "MP";
+        a=99;
+        System.out.println(str);
+        System.out.println(a);
+    }
+}
