d7315bd34a5666944cc2cb5ad24e0c58e831267a
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-21 15:56:41 +0530

    variable initialization

---
+++ src/com/mantosh/variable_declaration/Lab3.java
@@ -0,0 +1,8 @@
+package com.mantosh.variable_declaration;
+
+public class Lab3 {
+    public static void main(String[] as) {
+    int a = 0; //removing = 0 will throw an error variable a might not have been initialized
+    System.out.println(a);
+    }
+}
