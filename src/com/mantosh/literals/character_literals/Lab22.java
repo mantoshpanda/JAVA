7fc7ffcb6789b09123d4bb5a8032c7f3060894da
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-26 10:58:21 +0530

    Character Literals

---
+++ src/com/mantosh/literals/character_literals/Lab22.java
@@ -0,0 +1,12 @@
+package com.mantosh.literals.character_literals;
+
+public class Lab22 {
+    public static void main (String... as) {
+        /* char ch1 = ''; //EMPTY CHARACTER LITERAL
+        char ch2 = ' '; //VALID STATEMENT
+        char ch3 = 'AB'; //STRING
+        char ch4 = '\'; //DOUBLE BACK SLASH
+        char ch5 = ''''; //EMPTY CHARACTER LITERAL
+        */
+    }
+}
---
+++ src/com/mantosh/literals/character_literals/Lab23.java
@@ -0,0 +1,10 @@
+package com.mantosh.literals.character_literals;
+
+public class Lab23 {
+    public static void main (String... as){
+        char ch1 = ' ';
+        //char ch2 = '    ';//Too many characters in character literal
+        char ch3 = '/';
+        System.out.println(ch3);
+    }
+}
