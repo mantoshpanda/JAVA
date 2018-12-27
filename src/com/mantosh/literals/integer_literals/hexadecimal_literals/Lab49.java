d990edb6bf06dea37d358d99056d74ed1679808e
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-27 16:56:19 +0530

    Hexadecimal Literals

---
+++ src/com/mantosh/literals/integer_literals/hexadecimal_literals/Lab49.java
@@ -0,0 +1,12 @@
+package com.mantosh.literals.integer_literals.hexadecimal_literals;
+
+public class Lab49 {
+    public static void main(String... arg){
+        int a=0xA;
+        int b=0X61;
+        int c=0Xface;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
