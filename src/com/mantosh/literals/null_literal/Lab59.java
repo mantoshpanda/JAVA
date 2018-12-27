9a549811bcd1132c1dd8c5a55f5063b76bbfa6de
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-27 15:21:07 +0530

    Null Literals

---
+++ src/com/mantosh/literals/null_literal/Lab59.java
@@ -0,0 +1,10 @@
+package com.mantosh.literals.null_literal;
+
+public class Lab59 {
+    static String str1;
+    public static void main(String... str){
+        String str2=null;
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
