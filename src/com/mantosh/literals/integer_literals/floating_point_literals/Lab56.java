2e9d77cb52a1c9c9a8d51133496cefde0c2cdbe5
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-27 14:32:52 +0530

    Scientific/Exponent Notation

---
+++ src/com/mantosh/literals/integer_literals/floating_point_literals/Lab56.java
@@ -0,0 +1,12 @@
+package com.mantosh.literals.integer_literals.floating_point_literals;
+
+public class Lab56 {
+    public static void main(String... str){
+        float f=129.8763e2f;
+        double d1=129.8763e+2;
+        double d2=12987.63e-2;
+        System.out.println(f);
+        System.out.println(d1);
+        System.out.println(d2);
+    }
+}
