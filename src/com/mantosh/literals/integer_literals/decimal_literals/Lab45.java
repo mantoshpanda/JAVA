339e3000acb4ebf6169c34e434a6445dd7c4db82
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-27 16:58:14 +0530

    Decimal Literals

---
+++ src/com/mantosh/literals/integer_literals/decimal_literals/Lab45.java
@@ -0,0 +1,7 @@
+package com.mantosh.literals.integer_literals.decimal_literals;
+
+public class Lab45 {
+    public static void  main(String... arg){
+        // System.out.println(9880979999);
+    }
+}
---
+++ src/com/mantosh/literals/integer_literals/decimal_literals/Lab47.java
@@ -0,0 +1,9 @@
+package com.mantosh.literals.integer_literals.decimal_literals;
+
+public class Lab47 {
+    public static void main(String... arg){
+        /* int a=099;
+        System.out.println(a);
+        */
+    }
+}
