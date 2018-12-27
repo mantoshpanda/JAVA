09d0ebc85cb0778b24c0a126b47fa883537eb560
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-27 15:00:15 +0530

    Hexadecimal Floating Point Literals

---
+++ src/com/mantosh/literals/integer_literals/floating_point_literals/Lab57.java
@@ -0,0 +1,16 @@
+package com.mantosh.literals.integer_literals.floating_point_literals;
+
+public class Lab57 {
+    public static void main(String... arg){
+        double d1=0XaP0;
+        System.out.println(d1);
+        double d2=0XaP1;
+        System.out.println(d2);
+        double d3=0XaP2;
+        System.out.println(d3);
+        double d4=0XA9.0P0;
+        double d5=0XA9.P0;
+        System.out.println(d4);
+        System.out.println(d5);
+    }
+}
---
+++ src/com/mantosh/literals/integer_literals/floating_point_literals/Lab58.java
@@ -0,0 +1,8 @@
+package com.mantosh.literals.integer_literals.floating_point_literals;
+
+public class Lab58 {
+    public static void main(String... arg){
+        //double d1=0Xa12.12; //malformed floating point literal
+        //System.out.println(d1);
+    }
+}
