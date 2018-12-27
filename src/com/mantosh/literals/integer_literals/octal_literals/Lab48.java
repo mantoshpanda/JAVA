65821a77a8677683bbc7ffd89d7044bde616a7a2
Mantosh Panda <pmantosh@pkrmsezdc.com>
2018-12-27 16:53:52 +0530

    Octal Literals

---
+++ src/com/mantosh/literals/integer_literals/octal_literals/Lab48.java
@@ -0,0 +1,10 @@
+package com.mantosh.literals.integer_literals.octal_literals;
+
+public class Lab48 {
+    public static void main(String...arg){
+        int a=1010;
+        int b=0101;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
